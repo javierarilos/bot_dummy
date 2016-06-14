9b5c872180ef21caadce47815c977c298aa49813
javierarilos <javier.arilos@gmail.com>
2016-06-14 06:20:45 +0200

    bot: chat page under / . small refactor

--- bot.js
+++ bot.js
@@ -7,23 +7,26 @@ bot.add('/', function (session) {
     session.send('Hello World');
 });
 
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.get('/health', function (req, res, next) {
-  res.send('health OK.');
-  next();
-});
-server.get('/chat', function (req, res, next) {
+// HTTP endpoint functions
+function getChatPage(req, res, next) {
   res.setHeader('Content-Type', 'text/html');
   //res.contentType = 'text/html';
   res.end("<html><title>x</title><iframe  width='500' height='400' src='https://webchat.botframework.com/embed/AAAA-ZZZZ-1111-9999-bbbb-yyyy-2222-7777?s=mb5BZTQ1sSI.cwA.1L8.eAP0nXGGFc4IuY6Uvr_U1Dotyiju7p2u5LrTLeeZzOw'></iframe></html>");
   next();
-});
-server.get('/', function (req, res, next) {
-  res.send('hello there!!!!!!');
+}
+
+function getHealth(req, res, next) {
+  res.send('health OK.');
   next();
-});
+}
+
+
+// Setup Restify Server
+var server = restify.createServer();
+server.get('/', getChatPage);
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.get('/health', getHealth);
+
 
 // Start server
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000
