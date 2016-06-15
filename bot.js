4b4f6ee498b2fad5209f3072b54246dbb4114f2a
javierarilos <javier.arilos@gmail.com>
2016-06-15 07:02:55 +0200

    helloBot learns your name

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var builder = require('botbuilder');
 
+// Bot Factory function.
 function getBot() {
   if (process.env.BOT_STDIN === 'true') { // Testing chat in console
     return new builder.TextBot();
@@ -8,10 +9,30 @@ function getBot() {
   }
 }
 
-function init(bot) {
-  bot.add('/', function (session) {
-      session.send('Hi, my name is Chati');
-  });
+function init(bot){
+  bot.add('/', [
+    function (session, args, next) {
+      if (!session.userData.name) {
+        session.beginDialog('/profile');
+      } else {
+        next();
+      }
+    },
+    function (session, results) {
+      session.send('Hello %s!', session.userData.name);
+    }
+  ]);
+  bot.add('/profile', [
+    function (session) {
+      builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+      session.userData.name = results.response;
+      session.endDialog();
+    }
+  ]);
 }
+
+
 exports.getBot = getBot;
 exports.init = init
