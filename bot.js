c28da3e2fb1aee8966294648154766ea67dfeadc
javierarilos <javier.arilos@gmail.com>
2016-06-16 11:15:47 +0200

    bot: two dialogs, profile registration and calendar questioning this one is LUIS

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ function getBot() {
   }
 }
 
+
 function init(bot){
   bot.add('/', [
     function (session, args, next) {
@@ -19,18 +20,44 @@ function init(bot){
       }
     },
     function (session, results) {
-      session.send('Hello %s!', session.userData.name);
+      session.beginDialog('/agenda')
     }
   ]);
+
   bot.add('/profile', [
     function (session) {
       builder.Prompts.text(session, 'Hi! What is your name?');
     },
-    function (session, results) {
+    function (session, results, next) {
       session.userData.name = results.response;
+      // session.endDialog();
+      // session.next();
+      next()
+    },
+  //   },]);
+  //
+  // bot.add('/cal_url', [
+    function (session) {
+      builder.Prompts.text(session, 'Hi! '+session.userData.name+', give me access to your calendar. Write your calendar URL.');
+    },
+    function (session, results, next) {
+      session.userData.calendarUrl = results.response;
+      next();
+    },
+    function (session, results, next) {
+      session.send('OK %s now I can tell you about your appointments :)', session.userData.name);
+
       session.endDialog();
     }
   ]);
+
+  var LUIS_URL = 'https://api.projectoxford.ai/luis/v1/application?id=85f7ae76-c768-4f0e-a1dd-f37d955ecc86&subscription-key=ca18b6de51b947af8869aa4b404160a1'
+  var agendaDialog = new builder.LuisDialog(LUIS_URL);
+  agendaDialog.on('list_agenda', builder.DialogAction.send('=== Para hoy:\n\t(8:00) comprar el pan \n\t(10:00) salir a correr \n\t(17:00) recoger a las niñas'));
+  agendaDialog.on('help', builder.DialogAction.send('Soy tu gestor de agenda, símplemente dime qué eventos quieres consultar.'));
+  agendaDialog.onDefault(builder.DialogAction.send("Lo siento, no te entiendo. Ya sabes que yo puedo enseñarte tu agenda."));
+
+  bot.add('/agenda', agendaDialog)
 }
 
 
